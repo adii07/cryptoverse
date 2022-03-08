12cbce12c6a48c3dff50cb4193bcd01b5c0c8755
adii07 <adityasharma5119@gmail.com>
2022-03-08 17:27:40 +0530

    Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Button, Menu, Typography, Avatar  } from "antd";
 import { Link } from "react-router-dom";
 import { HomeOutlined, MoneyCollectOutlined,BulbOutlined,FundOutlined,MenuOutlined } from "@ant-design/icons/lib/icons";
-import icon from '../images/cryptocurrency.png'
+import icon from '../images/cryptocurrency.png';
 const Navbar =() =>{
     return(
         <div className="nav-container">
@@ -11,9 +11,21 @@ const Navbar =() =>{
                 <Typography.Title level={2} className="logo">
                     <Link to='/'>Cryptoverse</Link>
                 </Typography.Title>
-                {/* <Button className="menu-control-container"></Button> */}
-
             </div>
+            <Menu theme="dark" >
+                <Menu.Item icon={<HomeOutlined/>}>
+                    <Link to='/'>Home</Link>
+                </Menu.Item>
+                <Menu.Item icon={<FundOutlined/>}>
+                    <Link to='/cryptocurrencies'>CryptoCurrencies</Link>
+                </Menu.Item>
+                <Menu.Item icon={<MoneyCollectOutlined/>}>
+                    <Link to='/exchanges'>Exchanges</Link>
+                </Menu.Item>
+                <Menu.Item icon={<BulbOutlined/>}>
+                    <Link to='/news'>News</Link>
+                </Menu.Item>
+            </Menu>
         </div>
     );
 }
