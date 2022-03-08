f5ae2d344afe4acd5fa81f6b6fdf7b52463b6f94
adii07 <adityasharma5119@gmail.com>
2022-03-08 15:13:15 +0530

    React Router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { Switch , Route , Link } from 'react-router-dom';
 import { Layout, Typography , Space } from "antd";
+import { Navbar } from "./components";
+
 
 const App = ()=>{
     return(
         <div>
             <div className="app">
                 <div className="navbar">
-                    
+                    <Navbar/>
                 </div>
                 <div className="main">
 
