55eb104baa143af18ad7ab61fce144878c6020c8
adii07 <adityasharma5119@gmail.com>
2022-03-09 00:07:49 +0530

    HomePage Layout

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,10 +1,20 @@
 import React from "react";
+import millify from "millify";
+import { Typography, Row, Col, Statistic } from "antd";
+import { Link } from "react-router-dom";
 
 const HomePage=()=>{
     return(
-        <div>
-
-        </div>
+       <>
+       <Typography.Title level={2} className="heading">Global Crypto Stats</Typography.Title>
+       <Row>
+           <Col span={12}><Statistic title="Total Crypto Currencies" value='5'/></Col>
+           <Col span={12}><Statistic title="Total Crypto Exchanges" value='5'/></Col>
+           <Col span={12}><Statistic title="Total Market Cap" value='5'/></Col>
+           <Col span={12}><Statistic title="Total 24h Volume" value='5'/></Col>
+           <Col span={12}><Statistic title="Total Market" value='5'/></Col>
+       </Row>
+       </>
     );
 }
 export default HomePage;
