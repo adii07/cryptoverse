3e2d6ee24fffd8afedd1c219fafcf3881e0a826a
adii07 <adityasharma5119@gmail.com>
2022-03-09 15:39:15 +0530

    cryptocurrencies page

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -3,12 +3,13 @@ import millify from "millify";
 import { Typography, Row, Col, Statistic } from "antd";
 import { Link } from "react-router-dom";
 import { useGetCryptosQuery } from "../services/cryptoAPI";
+import {Cryptocurrencies,News} from '../components';
 
 const HomePage=()=>{
-    const{data, isFetching}=useGetCryptosQuery();
+    const{data, isFetching}=useGetCryptosQuery(10);
     const globalStats=data?.data?.stats;
     if(isFetching) return "Loading......";
-    console.log(data);
+    // console.log(data);
     return(
        <>
        <Typography.Title level={2} className="heading">Global Crypto Stats</Typography.Title>
@@ -19,6 +20,16 @@ const HomePage=()=>{
            <Col span={12}><Statistic title="Total 24h Volume" value={millify(globalStats.total24hVolume)} /></Col>
            <Col span={12}><Statistic title="Total Market" value={millify(globalStats.totalMarkets)} /></Col>
        </Row>
+       <div className="home-heading-container">
+           <Typography.Title level={2} className="home-title">Top 10 Cryptocurrencies in the world</Typography.Title>
+           <Typography.Title level={3} className="show-more"><Link to="/cryptocurrencies">Show more</Link></Typography.Title>
+       </div>
+       <Cryptocurrencies simplified />
+       <div className="home-heading-container">
+           <Typography.Title level={2} className="home-title">Latest Crypto News</Typography.Title>
+           <Typography.Title level={3} className="show-more"><Link to="/news">Show more</Link></Typography.Title>
+       </div>
+       <News simplified/>
        </>
     );
 }
